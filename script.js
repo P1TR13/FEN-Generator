d82b548840ba6ca40bcb77b03b702774b757ed05
P1TR13 <pawel.michalowski@onet.pl>
2021-12-21 14:34:09 +0100

    Added FEN Generator

--- script.js
+++ script.js
@@ -13,10 +13,24 @@ info.addEventListener('click', function onOpen() {
 
 let board = document.querySelector("#board")
 let last = document.querySelector("#last")
+var lastPiece = []
+let lastID
 
 let chessPieces = ["♔", "♕", "♖", "♗", "♘", "♙", "♚", "♛", "♜", "♝", "♞", "♟"]
+let notation = ["K", "Q", "R", "B", "N", "P", "k", "q", "r", "b", "n", "p"]
 
 let boxes = [['1','1','1','1','1','1','1','1','1','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','1','1','1','1','1','1','1','1','1']]
+
+let infoPieces = [['1','1','1','1','1','1','1','1','1','1'],
             ['1','0','0','0','0','0','0','0','0','1'],
             ['1','0','0','0','0','0','0','0','0','1'],
             ['1','0','0','0','0','0','0','0','0','1'],
@@ -66,8 +80,8 @@ function createChessPieces() {
         let div1 = document.createElement("div")
         let div2 = document.createElement("div")
         
-        div1.setAttribute("id", "white" + i)
-        div2.setAttribute("id", "black" + i)
+        div1.setAttribute("id", "w" + i)
+        div2.setAttribute("id", "b" + (i + 6))
         div1.classList.add("pieceButton")
         div2.classList.add("pieceButton")
         div1.innerHTML = chessPieces[i]
@@ -78,6 +92,85 @@ function createChessPieces() {
     }
 }
 
+function button() {
+    let buttons = document.querySelectorAll (".pieceButton")
+    buttons.forEach(button => button.addEventListener("click", function() {
+        if (lastPiece.length !== 0) {
+            let last = lastPiece.pop()
+            if (last !== button) {
+                last.classList.remove("border", "pieceButton2")
+                last.classList.add("pieceButton")
+            }
+            
+        }
+        
+        if (button.classList.contains("border")) {
+            button.classList.remove("border", "pieceButton2")
+            button.classList.add("pieceButton")
+            lastID = ""
+        } else {
+            button.classList.add("border", "pieceButton2")
+            button.classList.remove("pieceButton")
+            lastPiece.push(button)
+            lastID = button.id
+        }
+        
+        
+    }))
+}
+
+let fen = ""
+let input = document.querySelector("#input")
+
+function changeFEN() {
+    let count = 0
+    fen = ""
+    for (let x = 1; x <= 8; x++) {
+        count = 0
+        for (let y = 1; y <= 9; y++) {
+            if (y === 9) {
+                if (count !== 0) {
+                    fen += count.toString()
+                }
+                fen += "/"
+            } else if (infoPieces[x][y] === "0") {
+                count++
+            } else {
+                if (count !== 0) {
+                    fen += count.toString()
+                }
+                count = 0
+                fen += infoPieces[x][y]
+            }
+        }
+    }
+    input.setAttribute("value", fen)
+}
+
+function putPiece() {
+    let squares = document.querySelectorAll(".square")
+    squares.forEach(s => s.addEventListener("click", function() {
+        let chessID
+        if (lastID !== "") {
+            if (lastID.length === 3) {
+                chessID = lastID[1] + lastID[2]
+            } else {
+                chessID = lastID[1]
+            }
+            if(s.innerHTML !== chessPieces[chessID]) {
+                s.innerHTML = chessPieces[chessID]
+                infoPieces[s.id[1]][s.id[2]] = notation[chessID]
+            } else {
+                s.innerHTML = ""
+                infoPieces[s.id[1]][s.id[2]] = "0"
+            }
+        }
+        changeFEN()
+    }))
+}
+
 createBoard()
 gettingBoxes()
 createChessPieces()
+button()
+putPiece()
